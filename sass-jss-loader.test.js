3b036b1174da036b7bf674006e637f07c9dc2858
Jon Sakas <jon.sakas@beatport.com>
2018-06-02 01:18:24 -0600

    Save work - start writing @import resolution

--- sass-jss-loader.test.js
+++ sass-jss-loader.test.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
+const path = require('path');
 let sassJssLoader = require('./sass-jss-loader');
 
 // bind query to loader, "query" is used in loader-utils as "options"
 sassJssLoader = sassJssLoader.bind({
   query: {
     debug: './test/module.js',
+    sass: {
+      includePaths: [
+        path.resolve(__dirname),
+      ]
+    }
   }
 })
 
@@ -19,12 +25,33 @@ describe('sass-jss-loader', () => {
   it('exports a default function that returns a JSS object of the compiled sass', () => {
     const jss = testModule.default();
     expect(jss.Button.background).toBe('red');
+    expect(jss.Button.padding).toBe('10px');
   });
 
   it('exports a themeFactory that allows you to swap out variables', () => {
     const jss = testModule.themeFactory({
       '$color-1': 'blue',
+      '$padding-1': '20px',
     })
     expect(jss.Button.background).toBe('blue');
+    expect(jss.Button.padding).toBe('20px');
+  });
+
+  /**
+   * TODO - https://github.com/postcss/postcss-scss/issues/90
+   * 
+      .Interpolated {
+        margin: #{$number}rem;
+      }
+   */
+  it.skip('works with sass interpolated strings', () => {
+    expect(testModule.default().Interpolated.margin).toBe('10rem');
+    expect(testModule.default({
+      '$number': 20,
+    }).Interpolated.margin).toBe('20rem');
   })
+
+  it('resolves variables from @import statements', () => {
+    expect(testModule.defaultSassVars).toHaveProperty('$imported-var');
+  });
 });
